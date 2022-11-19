48bbfeed4bcb03a8641d630d2b922e60b5e74dea
Zeboxon <113965402+talabazebo2005@users.noreply.github.com>
2022-11-19 10:41:19 +0500

    Add files via upload

---
+++ Elektron Tasbeh/app.js
@@ -0,0 +1,10 @@
+document.querySelector('.button').addEventListener('click', function() {
+    k = document.querySelector('.ekran')
+    k.value = parseInt(k.value) + 1
+})
+
+document.querySelector('.delete').addEventListener('click', function() {
+    k = document.querySelector('.ekran')
+    tozalash = 0;
+    k.value = tozalash;
+})
